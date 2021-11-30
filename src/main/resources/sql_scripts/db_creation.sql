bdc681c9da45a8f392287d2cafc35c3df74000a1
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-11-30 12:51:02 +0300

    change(DB): change DB structure

--- src/main/resources/sql_scripts/db_creation.sql
+++ src/main/resources/sql_scripts/db_creation.sql
@@ -1,38 +1,49 @@
-DROP TABLE IF EXISTS book_to_category CASCADE;
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS book_categories;
+DROP TABLE IF EXISTS book_genres CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS clients CASCADE;
+DROP TABLE IF EXISTS libraries CASCADE;
+DROP TABLE IF EXISTS genres CASCADE;
+
+CREATE TABLE libraries
+(
+	id             bigserial PRIMARY KEY,
+	phone_number   varchar(20) NOT NULL,
+	email          varchar(50) NOT NULL,
+	address_street varchar(50) NOT NULL,
+	address_house  varchar(10) NOT NULL,
+	city           varchar(20) NOT NULL
+);
 
 CREATE TABLE clients
 (
-	id         bigserial PRIMARY KEY,
-	first_name varchar(50) NOT NULL,
-	surname    varchar(50) NOT NULL,
-	age        int         NOT NULL
+	id            bigserial PRIMARY KEY,
+	first_name    varchar(50)  NOT NULL,
+	last_name     varchar(50)  NOT NULL,
+	password      varchar(100) NOT NULL,
+	date_of_birth DATE         NOT NULL
+);
+
+CREATE TABLE genres
+(
+	id    bigserial PRIMARY KEY,
+	genre varchar(50) NOT NULL
 );
 
 CREATE TABLE books
 (
 	id               bigserial PRIMARY KEY,
-	title            varchar(50) NOT NULL,
-	author           varchar(50) NOT NULL,
-	publication_date DATE        NOT NULL
-		CONSTRAINT max_publication_date CHECK ( publication_date <= NOW() ) DEFAULT NOW(),
-	publication      int         NOT NULL                                   DEFAULT 1
-		CONSTRAINT positive_publications_only CHECK ( publication >= 1 ),
-	age_restriction  int         NOT NULL                                   DEFAULT 0
-		CONSTRAINT min_age_restriction CHECK ( books.age_restriction >= 0 ),
-	client_id        int REFERENCES clients ( id )
+	title            varchar(50)  NOT NULL,
+	author           varchar(50)  NOT NULL,
+	description      varchar(500) NOT NULL DEFAULT 'No description',
+	edition          int          NOT NULL DEFAULT 1,
+	publishing_house varchar(50)  NOT NULL,
+-- 	cover_image BOOK'S COVER IMAGE
+	library          int REFERENCES libraries ( id ),
+	tenant           int REFERENCES clients ( id )
 );
 
-CREATE TABLE book_categories
+CREATE TABLE book_genres
 (
-	id   bigserial PRIMARY KEY,
-	name varchar(20) NOT NULL
-);
-
-CREATE TABLE book_to_category
-(
-	book_id     int REFERENCES books ( id ),
-	category_id int REFERENCES book_categories ( id )
-);
+	book_id  int REFERENCES books ( id ),
+	genre_id int REFERENCES genres ( id )
+)
